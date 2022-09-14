23ad5b1f24d96367327e58c102cde50efcab2dbd
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-09-14 15:11:52 +0200

    Remove unused functions
    Remove unused function trim_time_axis
    
    Remove unused function random_subsample

--- matfact/matfact/data_generation/masking.py
+++ matfact/matfact/data_generation/masking.py
@@ -1,14 +1,6 @@
 import numpy as np
 
 
-def random_subsample(Y, N, seed=42):
-
-    np.random.seed(seed)
-    idx = np.random.choice(range(Y.shape[0]), replace=False, size=N)
-
-    return Y[idx]
-
-
 def simulate_mask(D, observation_proba, memory_length, level, seed=42):
     """Simulate a missing data mask."""
 
@@ -36,37 +28,3 @@ def simulate_mask(D, observation_proba, memory_length, level, seed=42):
         observed_values[r <= p, t + 1] = D[r <= p, t + 1]
 
     return mask
-
-
-def trim_time_axis(X, dt, t_min=None, t_max=None):
-
-    t_min = t_min if t_min is not None else 16
-    t_max = t_max if t_max is not None else 100
-
-    # Dots per year.
-    dpy = 12 // dt
-
-    t_min_idx = int((t_min - 16) * dpy)
-    t_max_idx = int((t_max - 16) * dpy + 1)
-
-    return X[:, t_min_idx:t_max_idx]
-
-
-def thresh_observation_count(X, min_n_obs=None, max_n_obs=None, meta=None):
-    """Control the number of observations per individual."""
-
-    nz = np.count_nonzero(X, axis=1)
-
-    if min_n_obs is not None and max_n_obs is not None:
-        to_keep = np.logical_and(nz >= min_n_obs, nz <= max_n_obs)
-
-    if min_n_obs is not None and max_n_obs is None:
-        to_keep = nz >= min_n_obs
-
-    if min_n_obs is None and max_n_obs is not None:
-        to_keep = nz <= max_n_obs
-
-    if meta is not None:
-        return X[to_keep], meta.iloc[to_keep]
-
-    return X[to_keep]
