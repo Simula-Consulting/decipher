4dc4b7e14794686e76bf754e33723d926aec9e3d
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-09-29 16:34:56 +0200

    Move test_train to test_system.py
    Separate tests that should not be included in coverage report from the other tests.

--- matfact/tests/test_experiment.py
+++ matfact/tests/test_experiment.py
@@ -1,11 +1,6 @@
-from itertools import product
-
 import numpy as np
 import pytest
 
-from examples.example import experiment as experiment1
-from examples.example_train_and_log import experiment as experiment2
-from matfact.data_generation import Dataset
 from matfact.experiments import (
     CMF,
     SCMF,
@@ -14,54 +9,6 @@ from matfact.experiments import (
     prediction_data,
     train_and_log,
 )
-
-
-def test_train(tmp_path):
-    """Run full system test"""
-    # Generate some data
-    dataset_params = {
-        "N": 1000,
-        "T": 50,
-        "rank": 5,
-        "sparsity_level": 6,
-    }
-    Dataset().generate(**dataset_params).save(tmp_path)
-
-    mlflow_tags = {
-        "Developer": "Developer Name",
-    }
-    # Params chosen semi-arbitrarily
-    # Number of epcoh is low to make runs fast
-    hyperparams = {
-        "rank": 5,
-        "lambda1": 10,
-        "lambda2": 10,
-        "lambda3": 100,
-    }
-    optimization_params = {
-        "num_epochs": 10,
-        "patience": 2,
-    }
-
-    for shift, weight, convolve in product([False, True], repeat=3):
-        experiment1(
-            hyperparams,
-            optimization_params,
-            shift,
-            weight,
-            convolve,
-            mlflow_tags=mlflow_tags,
-            dataset_path=tmp_path,
-        )
-        experiment2(
-            hyperparams,
-            optimization_params,
-            shift,
-            weight,
-            convolve,
-            mlflow_tags=mlflow_tags,
-            dataset_path=tmp_path,
-        )
 
 
 def test_model_input_not_changed():
---
+++ matfact/tests/test_system.py
@@ -0,0 +1,56 @@
+"""Full system tests.
+
+Should not be included in coverage reporting, as they simply run a bunch of code."""
+from itertools import product
+
+from examples.example import experiment as experiment1
+from examples.example_train_and_log import experiment as experiment2
+from matfact.data_generation import Dataset
+
+
+def test_train(tmp_path):
+    """Run full system test"""
+    # Generate some data
+    dataset_params = {
+        "N": 1000,
+        "T": 50,
+        "rank": 5,
+        "sparsity_level": 6,
+    }
+    Dataset().generate(**dataset_params).save(tmp_path)
+
+    mlflow_tags = {
+        "Developer": "Developer Name",
+    }
+    # Params chosen semi-arbitrarily
+    # Number of epcoh is low to make runs fast
+    hyperparams = {
+        "rank": 5,
+        "lambda1": 10,
+        "lambda2": 10,
+        "lambda3": 100,
+    }
+    optimization_params = {
+        "num_epochs": 10,
+        "patience": 2,
+    }
+
+    for shift, weight, convolve in product([False, True], repeat=3):
+        experiment1(
+            hyperparams,
+            optimization_params,
+            shift,
+            weight,
+            convolve,
+            mlflow_tags=mlflow_tags,
+            dataset_path=tmp_path,
+        )
+        experiment2(
+            hyperparams,
+            optimization_params,
+            shift,
+            weight,
+            convolve,
+            mlflow_tags=mlflow_tags,
+            dataset_path=tmp_path,
+        )
