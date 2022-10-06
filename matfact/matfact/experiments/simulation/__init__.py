365c1da5155ee0bd1341b28c11ed2b54df06e9e0
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-10-06 14:53:45 +0200

    chore: remove unused train_test_split

--- matfact/matfact/experiments/simulation/__init__.py
+++ matfact/matfact/experiments/simulation/__init__.py
@@ -1,9 +1,7 @@
 from .dataset import prediction_data
-from .train_test import train_test_split
 from .weights import data_weights
 
 __all__ = [
     "prediction_data",
-    "train_test_split",
     "data_weights",
 ]
