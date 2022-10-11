8d7d4f794644d1bbb923def46b88cf5aa3bfc753
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-10-11 10:59:27 +0200

    WIP: command line

--- matfact/examples/example_command_line.py
+++ matfact/examples/example_command_line.py
@@ -4,12 +4,25 @@ import pathlib
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import yaml
+
+from matfact.experiments.predict.clf_tree import ClassificationTree
 
 matplotlib.use("MACOSX")
 import logging
 
-from matfact.experiments.algorithms.risk_prediction import predict_proba, predict_state
+from matfact.experiments.algorithms.risk_prediction import (
+    fill_history,
+    predict_proba,
+    predict_state,
+)
 from matfact.experiments.simulation.dataset import _t_pred
+
+
+class ConfigurationError(Exception):
+    """The configuration is not valid."""
+
+    pass
 
 
 def plot_traj(states, ignore_all_normal=True):
@@ -22,8 +35,8 @@ def plot_traj(states, ignore_all_normal=True):
 
 def plot_traj(states, ignore_all_normal=True):
     matplotlib.use("MACOSX")
-    mask = np.any(states != 1, axis=1)
-    plt.imshow(states[np.nonzero(mask)].T, aspect="auto", interpolation=None)
+    mask = np.nonzero(np.any(states != 1, axis=1))
+    plt.imshow(states[mask], aspect="auto", interpolation=None)
     plt.show()
 
 
@@ -35,12 +48,29 @@ def main():
     parser.add_argument(
         "observation_array", type=pathlib.Path, help="path to the observation array."
     )
+    parser.add_argument("--config", type=pathlib.Path, help="config file")
     parser.add_argument("--theta", type=float, default=5)
     parser.add_argument("--num", type=int, default=1)
     parser.add_argument("--step", type=int, default=1)
+    parser.add_argument("--plot", action="store_true")
 
     logging.basicConfig(level=logging.INFO)
     args = parser.parse_args()
+
+    if not all(
+        file is None or file.is_file()
+        for file in [args.model_array, args.observation_array, args.config]
+    ):
+        raise ValueError("Supply files, not directories!")
+
+    predicted_states_path = args.observation_array.with_stem(
+        args.observation_array.stem + "_predicted"
+    )
+    if args.plot:
+        predicted_states = np.load(predicted_states_path)
+        plot_traj(predicted_states)
+        exit()
+
     try:
         model_array = np.load(args.model_array)
         observation_array = np.load(args.observation_array)
@@ -54,23 +84,34 @@ def main():
             "{model_array.shape} vs. {observation_array.shape}"
         )
 
-    number_of_individuals, time_steps = model_array.shape
+    config = {}
+    if args.config:
+        with open(args.config) as config_file:
+            config.update(yaml.full_load(config_file))
 
-    predicted_states = np.empty((args.num, number_of_individuals))
-    time_to_predict = _t_pred(observation_array, prediction_rule="last_observed")
-    if np.max(time_to_predict + args.num * args.step > time_steps):
-        raise ValueError(
-            "The given parameters would imply predicting outside of the training interval."
-        )
+    if config.get("thresholds") and config.get("number_of_states"):
+        if len(config["thresholds"]) != config["number_of_states"]:
+            raise ConfigurationError(
+                "The number of threshold values does not match the number of states."
+            )
 
-    for i in range(args.num):
-        probabilities = predict_proba(
-            observation_array, model_array, time_to_predict + i * args.step, args.theta
-        )
-        predicted_states[i] = predict_state(probabilities, None)
+    estimator = (
+        ClassificationTree(thresholds)
+        if (thresholds := config.get("thresholds"))
+        else None
+    )
 
-    print(np.diff(predicted_states, axis=0))
-    plot_traj(predicted_states)
+    predicted_states = fill_history(
+        observation_array,
+        model_array,
+        args.theta,
+        estimator=estimator,
+        use_predictions_as_observations=config.get(
+            "use_prediction_as_observations", False
+        ),
+    )
+    np.save(predicted_states_path, predicted_states)
+    # plot_traj(predicted_states)
 
 
 if __name__ == "__main__":
