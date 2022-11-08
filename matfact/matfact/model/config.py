b97bc813df61bdccb7f886c5e2f7bb6f9b83e4e6
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-11-08 09:48:24 +0100

    Transition from pydantic to dataclass

--- matfact/matfact/model/config.py
+++ matfact/matfact/model/config.py
@@ -1,12 +1,18 @@
-from pydantic import BaseModel
+from dataclasses import dataclass, field
+from typing import Callable
+
+import numpy.typing as npt
 
 from matfact import settings
+from matfact.model.factorization.utils import finite_difference_matrix
+from matfact.model.factorization.weights import data_weights
 
 
-class ModelConfig(BaseModel):
+@dataclass
+class ModelConfig:
     """Configuration class for the MatFact model."""
 
-    shift_budget: list[int] = []
+    shift_budget: list[int] = field(default_factory=list)
 
     lambda1: float = 1.0
     lambda2: float = 1.0
@@ -17,3 +23,6 @@ class ModelConfig(BaseModel):
 
     learning_rate: float = 0.001
     number_of_states: int = settings.default_number_of_states
+
+    difference_matrix_getter: Callable[[int], npt.NDArray] = finite_difference_matrix
+    weight_matrix_getter: Callable[[npt.NDArray], npt.NDArray] = data_weights
