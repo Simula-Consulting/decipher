f56fbe0622469d4cbe78dc634cfaa8b56f0cb8ce
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-11-08 10:13:12 +0100

    Add weight getter

--- matfact/matfact/model/config.py
+++ matfact/matfact/model/config.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from typing import Callable
 
@@ -6,6 +7,26 @@ import numpy.typing as npt
 from matfact import settings
 from matfact.model.factorization.utils import finite_difference_matrix
 from matfact.model.factorization.weights import data_weights
+
+
+class WeightGetter(ABC):
+    @abstractmethod
+    def __call__(self, X: npt.NDArray) -> npt.NDArray:
+        ...
+
+    is_identity: bool = False
+
+
+class IdentityWeighGetter(WeightGetter):
+    def __call__(self, X: npt.NDArray) -> npt.NDArray:
+        return X != 0
+
+    is_identity = True
+
+
+class DataWeightGetter(WeightGetter):
+    def __call__(self, X: npt.NDArray) -> npt.NDArray:
+        return data_weights(X)
 
 
 @dataclass
@@ -25,4 +46,4 @@ class ModelConfig:
     number_of_states: int = settings.default_number_of_states
 
     difference_matrix_getter: Callable[[int], npt.NDArray] = finite_difference_matrix
-    weight_matrix_getter: Callable[[npt.NDArray], npt.NDArray] = data_weights
+    weight_matrix_getter: type[WeightGetter] = DataWeightGetter
