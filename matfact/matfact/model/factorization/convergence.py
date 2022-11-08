b518001ec3109df2f0a7ab365f2a154ef0040999
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-11-08 15:55:08 +0100

    Remove old convergence monitor

--- matfact/matfact/model/factorization/convergence.py
+++ matfact/matfact/model/factorization/convergence.py
@@ -8,13 +8,6 @@ from matfact.settings import (
 )
 
 
-def convergence_monitor(M, error_tol=1e-4):
-    """Track convergence of the matrix completion process by measuring the
-    difference between consecutive estimates.
-    """
-    return MonitorFactorUpdate(M=M, tol=error_tol)
-
-
 class ConvergenceMonitor:
     def __init__(
         self,
@@ -48,52 +41,3 @@ class ConvergenceMonitor:
             should_update = i > self.patience and i % self.epochs_per_val == 0
             if should_update and self._update() < self.tolerance:
                 break
-
-
-class MonitorFactorUpdate:
-    def __init__(self, M, tol=1e-6):
-
-        self.M = M
-        self.tol = tol
-
-        self.n_iter_ = 0
-        self.update_ = []
-        self.convergence_rate_ = []
-
-    def _should_stop(self, M_new):
-
-        update = float(
-            np.linalg.norm(M_new - self.M) ** 2 / np.linalg.norm(self.M) ** 2
-        )
-
-        if np.isnan(update):
-            raise ValueError("Update value is NaN")
-
-        self.update_.append(update)
-
-        return update < self.tol
-
-    def track_convergence_rate(self, M_new):
-
-        self.Mpp = self.Mp
-        self.Mp = self.M
-        self.M = M_new
-
-        a = np.linalg.norm(self.M - self.Mp)
-        b = np.linalg.norm(self.Mp - self.Mpp) + 1e-12
-        self.convergence_rate_.append(a / b)
-
-    def converged(self, M_new):
-
-        should_stop = self._should_stop(M_new)
-
-        if self.n_iter_ > 0:
-            self.track_convergence_rate(M_new=M_new)
-
-        else:
-            self.Mp = self.M
-            self.M = M_new
-
-        self.n_iter_ += 1
-
-        return should_stop
