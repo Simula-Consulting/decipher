95c154206312b599f9ee5d166691624e5300b13e
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-12-07 12:46:07 +0100

    Remove hypothesis tests.
    Note: the tests should ideally work, but at the moment, our threshold solver does not
    guarantee the correcdt solution in all cases.

--- matfact/tests/test_threshold.py
+++ matfact/tests/test_threshold.py
@@ -1,56 +1,11 @@
-import itertools
-
 import numpy as np
 import pytest
-from hypothesis import given, note
-from hypothesis import strategies as st
-from hypothesis.extra.numpy import arrays
-from sklearn.metrics import matthews_corrcoef
 
 from matfact.model.predict.classification_tree import (
     ClassificationTree,
     Init,
     estimate_probability_thresholds,
 )
-
-
-@st.composite
-def probabilities(draw, min_samples=1, max_samples=10, min_states=2, max_states=5):
-    return draw(
-        arrays(
-            float,
-            st.tuples(
-                st.integers(min_value=min_samples, max_value=max_samples),
-                st.integers(min_value=min_states, max_value=max_states),
-            ),
-            elements=st.floats(min_value=0, max_value=1),
-        )
-        .filter(lambda p: np.all(np.any(p, axis=1)))  # Finite probabilities
-        .map(lambda p: p / np.sum(p, axis=1)[:, None])  # Normalize probabilities
-    )
-
-
-@st.composite
-def probability_and_correct(
-    draw, min_samples=1, max_samples=10, min_states=2, max_states=5
-):
-    probability_array = draw(
-        probabilities(
-            min_samples=min_samples,
-            max_samples=max_samples,
-            min_states=min_states,
-            max_states=max_states,
-        )
-    )
-    number_of_samples, number_of_states = probability_array.shape
-    fake_correct = draw(
-        arrays(
-            int,
-            number_of_samples,
-            elements=st.integers(min_value=1, max_value=number_of_states),
-        )
-    )
-    return probability_array, fake_correct
 
 
 @pytest.mark.parametrize(
@@ -121,60 +76,3 @@ def test_segmented_classification_tree_estimation(
             f"Thresholds too big. Got {clf.thresholds}, required max {max_thresholds}"
             f"Predicted {clf.predict(np.array(probabilities), age_segments)}."
         )
-
-
-# The optimization objective, Matthew's correlation coefficient, is not strictly larger
-# for better predictions.
-# Consider, for example, the case where the correct states are
-# [1, 1, 1]
-# Then, any prediction will give a score of 0.0 (out of -1 to 1)
-# >>> for predict in itertools.product(range(3), repeat=3):
-# >>>     assert matthews_corrcoef([1,1,1], predict) == 0.0
-# Therefore, we cannot optimize the thresholds for these cases.
-@pytest.mark.skip(
-    reason=(
-        "The score function, MCC, is not strictly larger "
-        "for correct prediction in all cases."
-    )
-)
-@given(probabilities(min_samples=10, max_samples=12))
-def test_classification_predictions_argmax(probabilities):
-    """Test that estimation works in the simple case were we set argmax as correct."""
-    correct = np.argmax(probabilities, axis=1) + 1  # Compensate for 1-indexed
-    number_of_samples = probabilities.shape[0]
-    age_segments = [0] * number_of_samples
-    clf = estimate_probability_thresholds(correct, probabilities, age_segments, 1)
-    note(f"Correct: {correct}")
-    note(f"Thresholds: {clf.thresholds}")
-    predicted = clf.predict(probabilities, age_segments)
-    note(f"MCC: {matthews_corrcoef(correct, predicted)}")
-    assert np.all(predicted == correct)
-
-
-@pytest.mark.skip(reason="The current solver does not guarantee an optimal solution")
-@given(probability_and_correct(min_samples=5, max_states=3))
-def test_all_partitions(probability_correct_pair):
-    """Test threshold estimation against exhaustive search of all possibilities."""
-    probabilities, correct = probability_correct_pair
-    age_segments = [0] * probabilities.shape[0]
-
-    # Find optimal score
-    threshold_limits = [(*set(p), 1) for p in probabilities.T[1:]]
-    clf = ClassificationTree()
-    best_score = -float("inf")
-    best_scoring_thresholds = []
-    for thresholds in itertools.product(*threshold_limits):
-        clf.set_params(thresholds=[thresholds])  # Only one segment, so wrap in [ ]
-        prediction = clf.predict(probabilities, age_segments)
-        if (score := matthews_corrcoef(correct, prediction)) >= best_score:
-            best_score = score
-            best_scoring_thresholds.append(thresholds)
-        note(f"{thresholds}, {prediction}, {score}")
-
-    # Estimate
-    clf_estimated = estimate_probability_thresholds(
-        correct, probabilities, age_segments, 1
-    )
-    prediction_estimated = clf_estimated.predict(probabilities, age_segments)
-    score_estimated = matthews_corrcoef(correct, prediction_estimated)
-    assert score_estimated == best_score
