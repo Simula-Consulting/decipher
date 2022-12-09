2a714d9739bbb56d2865952f74b5ea7cf3a52eeb
anders0204 <anders0204@gmail.com>
2022-12-09 13:41:23 +0100

    Refactor: add typing and docstrings

--- matfact/matfact/model/factorization/weights.py
+++ matfact/matfact/model/factorization/weights.py
@@ -1,9 +1,9 @@
-from typing import Sequence
+from typing import Sequence, Tuple
 
 import numpy as np
 import numpy.typing as npt
+import scipy
 import tensorflow as tf
-from scipy.linalg import svd
 from tensorflow.keras.constraints import Constraint
 from tensorflow.math import log, reduce_sum, sigmoid
 
@@ -34,61 +34,34 @@ def data_weights(
     return weight_matrix
 
 
-def propensity_weights(
-    observed_data_matrix: npt.NDArray[np.int_],
-    lr=0.1,
-    n_iter=1000,
-    tau=None,
-    gamma=None,
-) -> npt.NDArray:
-    """Construct weight matrix scaled by the inverse propensity score.
+class Projection(Constraint):
+    """Constraint class implementing both constraints on the optimization variable.
 
-    Solves the Bernoulli maximum likelihood problem (Eq. (3) in Ma & Chen (2019))
-    by minimizing a loss function defined as the opposite of the maximum likelihood.
+    The two constraints are:
+       1. Element norm: Individual entries of A are bounded by some value gamma:
+          |A|_max <= gamma
+       2. Nuclear norm: The nuclear norm is bounded by some value tau:
+          |A|_* <= tau * sqrt(m*n)
+
+    Tau and gamma are user specified.
     """
-    M = np.zeros_like(observed_data_matrix)
-    M[observed_data_matrix != 0] = 1
-    M = tf.cast(M, dtype=tf.float32)
-    M_one_mask = M == 1
-    M_zero_mask = M == 0
 
-    m, n = M.shape
-    tau_mn = (tau or settings.DEFAULT_TAU) * np.sqrt(m * n)
-    gamma = settings.DEFAULT_GAMMA
-
-    constraints = BothConstraints(tau_mn=tau_mn, gamma=gamma)
-
-    A = tf.Variable(tf.random.uniform(M.shape), constraint=constraints)
-    optimizer = tf.optimizers.Adam(learning_rate=lr)
-
-    def loss():
-        term1 = tf.reduce_sum(log(sigmoid(A.numpy()[M_one_mask])))
-        term2 = reduce_sum(log(1 - sigmoid(A[M_zero_mask])))
-        return -(term1 + term2)
-
-    for _ in range(n_iter):
-        optimizer.minimize(loss, var_list=[A])
-
-    propensity_matrix = sigmoid(A)
-    return (M / propensity_matrix).numpy()
-
-
-class BothConstraints(Constraint):
-    def __init__(self, tau_mn, gamma):
+    def __init__(self, tau_mn: float, gamma: float):
         self.tau_mn = tau_mn
         self.gamma = gamma
 
-    def element_norm(self, _A):
+    def element_norm(self, _A: tf.Tensor) -> tf.Tensor:
         return tf.clip_by_value(
             _A, clip_value_max=self.gamma, clip_value_min=-self.gamma
         )
 
-    def _simplex_projection(self, s):
+    def _simplex_projection(
+        self, s: npt.NDArray[np.float32]
+    ) -> npt.NDArray[np.float32]:
         """Projection onto the unit simplex."""
         if np.sum(s) <= self.tau_mn and np.alltrue(s >= 0):
-            # no need to project!
             return s
-        # get the array of cumulative sums of a sorted (decreasing) copy of s
+        # get the array of cumulative sums of a sorted (decreasing) copy of v
         u = np.sort(s)[::-1]
         cssv = np.cumsum(u)
 
@@ -96,19 +69,80 @@ class BothConstraints(Constraint):
         rho = np.nonzero(u * np.arange(1, len(u) + 1) > (cssv - self.tau_mn))[0][-1]
         # compute the Lagrange multiplier associated to the simplex constraint
         theta = (cssv[rho] - self.tau_mn) / (rho + 1)
-        # compute the projection by thresholding s using theta
+        # compute the projection by thresholding v using theta
         return np.maximum(s - theta, 0)
 
-    def _nuclear_projection(self, s, U, V):
+    def _nuclear_projection(
+        self,
+        s: npt.NDArray[np.float32],
+        U: npt.NDArray[np.float32],
+        V: npt.NDArray[np.float32],
+    ) -> npt.NDArray[np.float32]:
         """Projection onto nuclear norm ball."""
         s = self._simplex_projection(s)
         return U.dot(np.diag(s).dot(V))
 
-    def nuclear_norm(self, _A):
-        U, s, V = svd(_A, full_matrices=False)
+    def nuclear_norm(self, _A: tf.Tensor) -> npt.NDArray[np.float32]:
+        U, s, V = scipy.linalg.svd(_A, full_matrices=False)
         return self._nuclear_projection(s, U, V)
 
-    def __call__(self, A):
-        A = A.numpy()
-        A = self.nuclear_norm(A)
-        return self.element_norm(A)
+    def __call__(self, A: tf.Tensor) -> tf.Tensor:
+        return self.element_norm(self.nuclear_norm(A))
+
+
+def get_one_zero_mask(
+    M: npt.NDArray[np.int_] | tf.Tensor,
+) -> Tuple[npt.NDArray[np.bool_], npt.NDArray[np.bool_]]:
+    return (M == 1), (M == 0)
+
+
+def calculate_propensity_scores(
+    M: tf.Tensor, learning_rate: float, n_iter: int, tau: float, gamma: float
+) -> tf.Tensor:
+    """Function to calculate a propensity score matrix based on a binary observation
+    matrix. The propensity weights are calculated from the maximum likelihood equation
+    (Eq. (3) in Ma & Chen (2019)) using projected gradient descent.
+    The projections are included as constraints on the variable 'A'.
+
+    Currently we are only considering using the sigmoid function as a link function as
+    this seems to be the general consensus, but it could in theory be any function.
+    """
+    M_one_mask, M_zero_mask = get_one_zero_mask(M)
+    m, n = M.shape
+    tau_mn = tau * np.sqrt(m * n)
+
+    constraints = Projection(tau_mn=tau_mn, gamma=gamma)
+
+    A = tf.Variable(tf.random.uniform(M.shape), constraint=constraints)
+    optimizer = tf.optimizers.Adam(learning_rate=learning_rate)
+
+    def loss():
+        term1 = reduce_sum(log(sigmoid(A[M_one_mask])))
+        term2 = reduce_sum(log(1 - sigmoid(A[M_zero_mask])))
+        return -(term1 + term2)
+
+    for _ in range(n_iter):
+        optimizer.minimize(loss, var_list=[A])
+
+    return sigmoid(A)
+
+
+def propensity_weights(
+    observed_data_matrix: npt.NDArray[np.int_],
+    learning_rate: float = 0.1,
+    n_iter: int = 100,
+    tau: float = settings.DEFAULT_TAU,
+    gamma: float = settings.DEFAULT_GAMMA,
+) -> npt.NDArray:
+    """Construct weight matrix scaled by the inverse propensity score.
+    The propensity scores are calculated from a binary map of the observation matrix.
+    """
+    M = np.zeros_like(observed_data_matrix)
+    M[observed_data_matrix != 0] = 1
+    M = tf.cast(M, dtype=tf.float32)
+
+    propensity_scores = calculate_propensity_scores(
+        M, learning_rate=learning_rate, n_iter=n_iter, tau=tau, gamma=gamma
+    )
+
+    return (M / propensity_scores).numpy()
