c562717714769680c58be672bd162c9c64a62946
Thorvald Molthe Ballestad <thorvald@simula.no>
2022-12-19 13:43:16 +0100

    Split tests into module specific

--- bokeh-demo/tests/test_backend.py
+++ bokeh-demo/tests/test_backend.py
@@ -13,16 +13,6 @@ from bokeh_demo.backend import (
 )
 from bokeh_demo.exam_data import ExamTypes
 from bokeh_demo.faker import get_inverse_mapping
-from bokeh_demo.frontend import get_position_list
-
-
-@given(st.lists(st.integers()))
-def test_get_position_list(list_of_numbers: list[int]) -> None:
-    position_list = get_position_list(list_of_numbers)
-
-    sorted_list_of_numbers = sorted(list_of_numbers)
-    for number, position in zip(list_of_numbers, position_list):
-        assert sorted_list_of_numbers[position] == number
 
 
 @st.composite
