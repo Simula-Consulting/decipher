20b1caa41a190c0e79107935bd481c414a6fca6a
anders0204 <anders0204@gmail.com>
2023-01-17 14:54:49 +0100

    Add docstrings

--- matfact/matfact/data_generation/processing.py
+++ matfact/matfact/data_generation/processing.py
@@ -43,16 +43,20 @@ class ScreeningDataProcessingPipeline:
         )
 
     def _add_dob_column(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Method to add date of birth do a dataframe based on a map from person id."""
         df[self.columns.dob.date] = df[self.columns.pid].map(self.dob_map)
         return df
 
     def _convert_to_datetime(self, df: pd.DataFrame, columns: list[str]) -> None:
+        """Method to convert string values to datetimes."""
+
         def datetime_conversion(x: str) -> pd.Timestamp:
             return pd.to_datetime(x, format="%d.%m.%Y")
 
         df[columns] = df[columns].apply(datetime_conversion)
 
     def _add_age_column(self, df: pd.DataFrame) -> None:
+        """Method to add a column with person age at screening time."""
         time_cols = self.columns.get_date_columns()
         self._convert_to_datetime(df, columns=time_cols)
         for col in (self.columns.cyt.date, self.columns.hist.date):
@@ -61,10 +65,12 @@ class ScreeningDataProcessingPipeline:
             ).apply(lambda x: x.days)
 
     def _add_risk_column(self, df: pd.DataFrame) -> None:
+        """Method to add a column with diagnosis as risk levels."""
         for screening in settings.processing.risk_maps.keys():
             df["risk"] = df[screening].map(settings.processing.risk_maps[screening])
 
     def _gather_information(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Method to prepare the dataframe by gathering all available information."""
         df = self._add_dob_column(df)
         self._add_age_column(df)
         self._add_risk_column(df)
@@ -73,12 +79,15 @@ class ScreeningDataProcessingPipeline:
     def _exclude_invalid_data(
         self, df: pd.DataFrame, min_n_tests: int = 2
     ) -> pd.DataFrame:
+        """Method to exlude invalid data by dropping columns with NaN values
+        or females with only 1 screening."""
         new_df = df.dropna()
         person_counts = new_df[self.columns.pid].value_counts()
         rejected_pids = person_counts[person_counts.values < min_n_tests].index
         return new_df[~new_df[self.columns.pid].isin(rejected_pids)]
 
     def prepare_data(self) -> pd.DataFrame:
+        """Method to create a processed DataFrame containing only relevant and valid data."""
         df = pd.DataFrame.copy(
             self.screening_data[self.columns.get_screening_columns()]
         )
@@ -99,10 +108,13 @@ class ScreeningDataProcessingPipeline:
         return self._exclude_invalid_data(df)
 
     def _find_n_bins(self, df: pd.DataFrame) -> int:
+        """Method to calculate the number of age-bins required,
+        based on the age span in the data and the desired number of months per bin."""
         age_min, age_max = df["age"].min(), df["age"].max()
         avg_days_per_month = 30.437
 
         def ceildiv(a: float, b: float) -> int:
+            """Function to perform ceiling division, opposite of floor division."""
             return int(-(a // -b))
 
         return ceildiv(
@@ -111,12 +123,15 @@ class ScreeningDataProcessingPipeline:
         )
 
     def _assign_age_bins(self, df: pd.DataFrame, n_bins: int) -> None:
+        """Method to assign results into age bins (columns) based on the age at time of screening."""
         _, bin_edges = np.histogram(df["age"], bins=n_bins)
         bin_edges[0] -= 1  # to make sure the youngest age is included
         indexes = np.arange(n_bins)
         df["bin"] = pd.cut(df["age"], bins=bin_edges, labels=indexes)
 
     def _assign_row_index(self, df: pd.DataFrame) -> dict[int, int]:
+        """Method to assign a unique row to every person.
+        Also returns this mapping for future reference."""
         individuals = sorted(df["PID"].unique())
         n_females = len(individuals)
         row_map = dict(zip(individuals, np.arange(n_females)))
@@ -126,6 +141,7 @@ class ScreeningDataProcessingPipeline:
     def _create_age_aligned_matrix(
         self, df: pd.DataFrame
     ) -> tuple[np.ndarray, dict[int, int]]:
+        """Method to turn a processed Dataframe into a numpy array."""
         n_bins = self._find_n_bins(df)
         self._assign_age_bins(df, n_bins)
         row_map = self._assign_row_index(df)
@@ -137,11 +153,13 @@ class ScreeningDataProcessingPipeline:
         return X, row_map
 
     def generate_observation_matrix(self) -> np.ndarray:
+        """Method to process the screening data and produce an age-aligned observation matrix."""
         self.processed_data = self.prepare_data()
         self.X, self.row_map = self._create_age_aligned_matrix(self.processed_data)
         return self.X
 
     def to_csv(self, location: str, **kwargs) -> None:
+        """Method to save the processed data to a .csv file."""
         if self.processed_data is None:
             self.prepare_data()
         self.processed_data.to_csv(location, **kwargs)  # type: ignore
