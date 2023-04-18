38e5d80b14c676ffb5264e84a83a575b06d386a1
anders0204 <anders0204@gmail.com>
2023-04-18 12:34:57 +0200

    Add HHMM pipeline

--- processing/processing/pipelines.py
+++ processing/processing/pipelines.py
@@ -5,14 +5,17 @@ from sklearn.pipeline import Pipeline
 from processing.transformers import (
     AgeAdder,
     AgeBinAssigner,
-    BirthdateAdder,
     DataSampler,
     DatetimeConverter,
+    FolkeregInfoAdder,
     InvalidRemover,
     RiskAdder,
+    RiskAdderHHMM,
     RowAssigner,
-    FolkeregInfoAdder
+    TestIndexAdder,
+    ToExam
 )
+
 
 def matfact_pipeline(
     *,
@@ -25,9 +28,12 @@ def matfact_pipeline(
     """Returns a sklearn type pipeline for processing the matfact screening data."""
     return Pipeline(
         [
-            ("birthdate_adder", FolkeregInfoAdder(birthday_file=birthday_file, death_column=False)),
+            (
+                "birthdate_adder",
+                FolkeregInfoAdder(birthday_file=birthday_file, death_column=False),
+            ),
             ("datetime_converter", DatetimeConverter()),
-            ("age_adder", AgeAdder()),
+            ("age_adder", AgeAdder(in_years=False)),
             ("risk_adder", RiskAdder()),
             ("invalid_remover", InvalidRemover(min_n_tests=min_n_tests)),
             ("data_sampler", DataSampler(max_n_females=max_n_females)),
@@ -35,4 +41,27 @@ def matfact_pipeline(
             ("row_assigner", RowAssigner(row_map_save_path=row_map_save_path)),
         ],
         verbose=verbose,
+    )
+
+
+def HHMM_pipeline(
+    *,
+    verbose: bool = True,
+    birthday_file: Path | None = None,
+):
+    """Returns a sklearn type Pipeline for processing data for HHMM."""
+    return Pipeline(
+        [
+            (
+                "folkereg_adder",
+                FolkeregInfoAdder(birthday_file=birthday_file, death_column=True),
+            ),
+            ("to_exam_converter", ToExam(fields_to_keep=["PID", "FOEDT", "is_dead"])),
+            ("datetime_converter", DatetimeConverter(columns=["exam_date"])),
+            ("age_adder", AgeAdder(in_years=True, target_columns=["exam_date"])),
+            ("risk_adder", RiskAdderHHMM()),
+            ("test_index", TestIndexAdder()),
+            ("invalid_remover", InvalidRemover(min_n_tests=0)),
+        ],
+        verbose=verbose
     )
