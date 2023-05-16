afe7ef694799d0277cbe81c5481a80f69f5f4401
anders0204 <anders0204@gmail.com>
2023-05-16 10:28:48 +0200

    Refactor file file saving and loading

--- bokeh-demo/examples/landing_page/main.py
+++ bokeh-demo/examples/landing_page/main.py
@@ -1,4 +1,3 @@
-
 from bokeh.io import curdoc
 from bokeh.models import TextInput, Button, CustomJS
 import pickle
@@ -6,16 +5,19 @@ from bokeh_demo.settings import settings
 
 text = TextInput(title="Text input", value="input your text here")
 
+
 def save_text(attr, old, new):
-    with open(settings.transfer_path / "text.txt", "wb") as f:
+    with open(settings.selected_pids_path, "wb") as f:
         pickle.dump(text.value, f)
 
 
-callback = CustomJS(args=dict(text=text.value, location=str(settings.transfer_path)), code="""
-window.location.href = "/pilot"
-""")
 go_button = Button(label="Go")
-go_button.js_on_click(callback)
+go_button_js_callback = CustomJS(
+    code="""
+window.location.href = "/pilot"
+"""
+)
+go_button.js_on_click(go_button_js_callback)
 
 # currently saving file on every change
 text.on_change("value", save_text)
