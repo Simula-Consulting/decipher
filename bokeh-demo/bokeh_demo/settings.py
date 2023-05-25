5e0abf4f070cf9cbb1d34896d429a6d720ae5f6c
anders0204 <anders0204@gmail.com>
2023-05-25 13:55:16 +0200

    Add data path in settings

--- bokeh-demo/bokeh_demo/settings.py
+++ bokeh-demo/bokeh_demo/settings.py
@@ -1,13 +1,20 @@
 import pathlib
 
-from pydantic import BaseSettings, DirectoryPath
+from pydantic import BaseModel, BaseSettings, DirectoryPath
 
+
+class DataPaths(BaseModel):
+    base_path: DirectoryPath = pathlib.Path(__file__).parents[2] / "processing" / "tests" / "test_datasets"
+    screening_data_path: pathlib.Path = base_path / "test_screening_data.csv"
+    dob_data_path: pathlib.Path = base_path / "test_dob_data.csv"
 
 class Settings(BaseSettings):
     number_of_epochs: int = 100
     dataset_path: DirectoryPath = pathlib.Path(__file__).parents[1] / "data/dataset1"
     transfer_path: DirectoryPath = pathlib.Path(__file__).parents[1] / "data/transfer/"
     selected_pids_path: pathlib.Path = transfer_path / "selected_pids.json"
+
+    data_paths: DataPaths = DataPaths()
 
     label_map: list[str] = ["", "Normal", "Low risk", "High risk", "Cancer"]
     color_palette: list[str] = ["#13D697", "#0B6BB3", "#FFB60A", "#EF476F"]
